8cf87ef39da823c84c280ed963f54e48d250c1d1
Martin Hatina <mhatina@redhat.com>
2017-07-26 14:40:26 +0200

    remove necessity of 'modules=1' for repo files

--- dnf/conf/config.py
+++ dnf/conf/config.py
@@ -1025,9 +1025,6 @@ class RepoConf(BaseConfig):
                          SelectionOption('priority', choices=('priority',),
                                          notimplemented=('roundrobin',)))
 
-        # modules options
-        self._add_option('modules', BoolOption(False))
-
     def _configure_from_options(self, opts):
         """Configure repos from the opts. """
 
